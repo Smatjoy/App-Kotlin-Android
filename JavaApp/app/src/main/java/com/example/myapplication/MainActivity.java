9658ad02957d0cd5c70705eb49afe2590d96cb79
tigerDer1 <153896945+tigerDer1@users.noreply.github.com>
2025-04-05 09:30:58 +0200

    cose

--- JavaApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ JavaApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +31,50 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+        private void initViewPager () {
+            ViewPager viewPager = findViewById(R.id.viewpager);
+            TabLayout tabLayout = findViewById(R.id.tab_layout);
+            ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+            // Add fragments to the adapter
+            viewPagerAdapter.addFragment(new SongFragment(), "Songs");
+            viewPagerAdapter.addFragment(new AlbumFragment(), "Albums");
+
+            // Set the adapter to the ViewPager
+            viewPager.setAdapter(viewPagerAdapter);
+            // Connect the TabLayout with the ViewPager
+            tabLayout.setupWithViewPager(viewPager);
+        }
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final ArrayList<Fragment> fragments;
+        private final ArrayList<String> titles;
+        public ViewPagerAdapter (@NonNull FragmentManager fm) {
+            super (fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        void addFragment (Fragment fragment, String title) {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+        @NonNull
+        @Override
+        public Fragment getItem (int position) {
+            return fragments.get(position);
+        }
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle (int position) {
+            return titles.get(position);
+        }
+    }
+
+
 }
