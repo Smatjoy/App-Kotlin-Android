c83249e0e483bb36f8bd3850589f4e450a8577a3
Mattia Samanni <smatjoy06@icloud.com>
2025-04-09 14:53:22 +0200

    episodio 5 completato

--- JavaApp/app/src/main/java/com/example/myapplication/PlayerActivity.java
+++ JavaApp/app/src/main/java/com/example/myapplication/PlayerActivity.java
@@ -1,7 +1,16 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.MainActivity.musicFiles;
+
 import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +18,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
 public class PlayerActivity extends AppCompatActivity {
 
+    TextView song_name, artist_name, duration_played, duration_total, shuffleBtn;
+    ImageView cover_art, nextBtn, backBtn, repeatBtn, prevBtn;
+    FloatingActionButton playPauseBtn;
+    SeekBar seekBar;
+    int position = -1;
+    static Uri uri;
+    static MediaPlayer mediaPlayer;
+    ArrayList<MusicFiles> listSongs = new ArrayList<>();
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +43,87 @@ public class PlayerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+        getIntenMethod();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mediaPlayer != null && fromUser)
+                {
+                    mediaPlayer.seekTo(progress * 1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        PlayerActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer != null)
+                {
+                    int nCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                    seekBar.setProgress(nCurrentPosition);
+                    duration_played.setText(formattedTime(nCurrentPosition));
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
+    private String formattedTime(int mCurrentPosition) {
+        String totalout = "";
+        String totalNew = "";
+        String seconds = String.valueOf(mCurrentPosition % 60);
+        String minutes = String.valueOf(mCurrentPosition / 60);
+        totalout = minutes + ":" + seconds;
+        totalNew = minutes + ":" + "0" + seconds;
+        if (seconds.length() == 1) {
+            return totalNew;
+        } else {
+            return totalout;
+        }
+    }
+
+    private void getIntenMethod() {
+        position = getIntent().getIntExtra("position", -1);
+        listSongs = musicFiles;
+        if (listSongs != null) {
+            playPauseBtn.setImageResource(R.drawable.ic_pause);
+            uri = Uri.parse(listSongs.get(position).getPath());
+        }
+        if (mediaPlayer != null)
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        } else {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        }
+        seekBar.setMax(mediaPlayer.getDuration() / 1000);
+    }
+
+    private void initViews() {
+        song_name = findViewById(R.id.song_name);
+        artist_name = findViewById(R.id.song_artist);
+        duration_played = findViewById(R.id.durationPlayed);
+        duration_total = findViewById(R.id.durationTotal);
+        cover_art = findViewById(R.id.cover_art);
+        nextBtn = findViewById(R.id.id_next);
+        prevBtn = findViewById(R.id.id_prev);
+        backBtn = findViewById(R.id.back_btn);
+        //shuffleBtn = findViewById(R.id.id_shuffle);
+        repeatBtn = findViewById(R.id.id_repeat);
+        playPauseBtn = findViewById(R.id.play_pause);
+        seekBar = findViewById(R.id.seekBar);
     }
 }
