5ab6124f022ca3543a90da05b6121da147840a47
tigerDer1 <153896945+tigerDer1@users.noreply.github.com>
2025-04-12 08:18:10 +0200

    Create FisherYatesShuffle.java
    this does nothing for now

---
+++ JavaApp/app/src/main/java/com/example/myapplication/FisherYatesShuffle.java
@@ -0,0 +1,20 @@
+/*TO DO Add Fisher Yates Shuffle*/
+
+
+package com.example.myapplication;
+
+import java.util.Random;
+
+    public class FisherYatesShuffle {
+        public static void shuffle(String[] array) {
+            Random rand = new Random();
+
+            for (int i = array.length - 1; i > 0; i--) {
+                int j = rand.nextInt(i + 1); // 0 <= j <= i
+                // Swap array[i] with array[j]
+                String temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+}
